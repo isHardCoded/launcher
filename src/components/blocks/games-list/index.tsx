8d4a248daafdcf947e08673ced59b52209d5e844
isHardCoded <ishardcoded@gmail.com>
2026-08-20 21:01:17 +0500

    feat: добавили обработку ошибок

--- src/components/blocks/games-list/index.tsx
+++ src/components/blocks/games-list/index.tsx
@@ -5,13 +5,20 @@ import { GAMES_SERVICE } from "@/services/games"
 
 export function GamesList() {
   const [games, setGames] = useState<Game[]>([])
+  const [error, setError] = useState<string | null | unknown>(null)
     
     useEffect(() => {
       async function GetGames() {
-        setGames(await GAMES_SERVICE.get())
+        try {
+          setGames(await GAMES_SERVICE.get())
+        } catch (e) {
+          setError(e)
+        }
       }
       GetGames()
     }, [])
+    
+  if (error) return <div className="text-center text-[14px] text-[#dadada] mt-5">Ошибка при получении игр</div>
     
   return <div className="mt-4 grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4">
           {games.map((game) => (
--- src/services/games.ts
+++ src/services/games.ts
@@ -1,7 +1,11 @@
 export const GAMES_SERVICE = {
   get: async () => {
-    const response = await fetch("http://localhost:3001/games")
-    const data = await response.json()
-    return data
+    try {
+      const response = await fetch("http://localhost:3001/games")
+      const data = await response.json()
+      return data
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
